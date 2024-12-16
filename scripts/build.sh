b2c475c56ccaa3f169f91dfd540c0d1162c1b1b2
Tyler M <tmart234@gmail.com>
2024-12-15 22:46:36 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,7 +32,7 @@ repository.apache-mynewt-nimble:
 # Nordic HAL
 repository.nordic-nrfx:
     type: github
-    vers: 3.9.0
+    vers: 85c444ee0b76272d8a074b82845f04e92b3253c4
     user: NordicSemiconductor
     repo: nrfx
     repository.defrepo.depth: 1
