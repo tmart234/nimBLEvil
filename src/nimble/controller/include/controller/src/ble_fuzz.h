b49f51e728294fedf0adfbdaba0fb14601f503e7
Tyler M <tmart234@gmail.com>
2025-01-20 17:57:29 -0700

    more patching

--- src/nimble/controller/include/controller/src/ble_fuzz.h
+++ src/nimble/controller/include/controller/src/ble_fuzz.h
@@ -7,7 +7,17 @@
 extern "C" {
 #endif
 
+/* Vendor-specific HCI commands */
+#define BLE_HCI_OGF_VENDOR                 0x3f
+#define BLE_HCI_OCF_RAW_TX                 0x01
+#define BLE_HCI_RAW_TX_OPCODE              (BLE_HCI_OGF_VENDOR << 10 | BLE_HCI_OCF_RAW_TX)
+#define BLE_HCI_CMD_RAW_TX_LEN             251
+
+/* Global fuzzing state */
 extern bool g_fuzzing_mode_enabled;
+extern bool g_phy_raw_tx_mode;
+
+/* Fuzzing API */
 void ble_fuzz_set_mode(bool enable);
 void ble_fuzz_inject_packet(const uint8_t *data, size_t len);
 
--- src/nimble/controller/src/ble_fuzz.c
+++ src/nimble/controller/src/ble_fuzz.c
@@ -1,18 +1,17 @@
-#include "ble_fuzz.h"
+#include "controller/ble_fuzz.h"
 #include "controller/ble_phy.h"
 #include "controller/ble_ll.h"
 
+bool g_phy_raw_tx_mode;
 bool g_fuzzing_mode_enabled;
 
-void ble_fuzz_set_mode(bool enable) 
-{
-    g_fuzzing_mode_enabled = enable;
-    ble_phy_set_fuzz_mode(enable);  // Hypothetical PHY fuzz API
+void ble_fuzz_init(void) {
+    g_fuzzing_mode_enabled = true;
+    g_phy_raw_tx_mode = true;
 }
 
-void ble_fuzz_inject_packet(const uint8_t *data, size_t len)
-{
-    if (g_fuzzing_mode_enabled) {
-        ble_ll_tx_raw_packet(data, len);  // Hypothetical LL raw TX
-    }
+void ble_fuzz_set_mode(bool enable) {
+    g_fuzzing_mode_enabled = enable;
+    g_phy_raw_tx_mode = enable;
+    ble_phy_disable_whiten(enable);  // Existing PHY function
 }
